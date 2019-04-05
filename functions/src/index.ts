1d095ead509d96b2b3485179374389d37be7f9ee
Jared Williams <jared@themovesapp.com>
2019-04-05 17:30:47 -0400

    Updated the API so that it only returns new events and deleted old ones if they are present

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,10 +4,25 @@ import * as admin from 'firebase-admin';
 admin.initializeApp()
 const firestore = admin.firestore()
 
-export const getAllEvents = functions.https.onRequest((req, res) => {
-    return firestore.collection('events').get().then(snap => {
-        res.status(200).send(convertSnapshotTimeStampsToSecondsSince1970(snap));
-    });
+export const getAllEvents = functions.https.onRequest( async (req, res) => {
+        const snap = await firestore.collection('events').get();
+        const onlyNewEvents = findAllNewDates(snap);
+        const batchWrite = firestore.batch()
+        
+        res.status(200).send(convertSnapshotTimeStampsToSecondsSince1970(onlyNewEvents));
+
+        // Basically here we are pushing only the new events back to the api
+        snap.docs.forEach(doc => {
+            const docId = doc.id;
+            const docData = doc.data();
+            
+            if (docData['timeStamp'] && new Date(docData['timeStamp']).getTime() < new Date().getTime()) {
+                const docRef = firestore.collection('events').doc(docId);
+                batchWrite.delete(docRef);
+            }
+        });
+
+        return batchWrite.commit();
 });
 
 export const getEventById = functions.https.onRequest((req, res) => {
@@ -18,20 +33,34 @@ export const getEventById = functions.https.onRequest((req, res) => {
     })
 });
 
-function convertSnapshotTimeStampsToSecondsSince1970(snap: FirebaseFirestore.QuerySnapshot): Array<Object> {
-    let returnValue = [];
+function convertSnapshotTimeStampsToSecondsSince1970(dates: Array<Object>): Array<Object> {
+    const returnValue = [];
+    dates.forEach(date => {
+        const tempObject = new Object();
+        Object.assign(tempObject, date);
 
-    snap.docs.forEach(doc => {
-
-        let newObject = {};
-        Object.assign(newObject, doc.data());
-
-        let oldTime = newObject['timeStamp'];
-        let newDateObject = new Date(oldTime).getTime() / 1000.0;
-
-        newObject['timeStamp'] = newDateObject;
-        returnValue.push(newObject);
+        if (date['timeStamp']) {
+            const msDate = date['timeStamp'];
+            tempObject['timeStamp'] = new Date(msDate).getTime() / 1000.0;
+            returnValue.push(tempObject);
+        }
     });
 
     return returnValue;
+}
+
+function findAllNewDates(snap: FirebaseFirestore.QuerySnapshot): Array<Object> {
+    const returnObject = new Array<Object>();
+    const currentDate = new Date().getTime();
+    let oldData = new Object();
+
+    snap.docs.forEach(doc => {
+        oldData = doc.data();
+
+        if (oldData['timeStamp'] && new Date(oldData['timeStamp']).getTime() > currentDate) {
+            returnObject.push(oldData);
+        }
+    });
+
+    return returnObject;
 }
